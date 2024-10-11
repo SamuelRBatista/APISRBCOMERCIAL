63d96cd84ea5baeb8567ac09941c368d8a9d757f
SamuelRBatista <samuel.r.batista@outlook.com>
2024-10-11 12:10:59 -0300

    feat: 11102024 => Implementação do contexto da base de dados

---
+++ SrbComercialInfrastructure/Data/DataContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using SrbComercialDomain.Entities;
+using static SrbComercialInfrastructure.Data.DataContext;
+
+
+namespace SrbComercialInfrastructure.Data
+{
+    public class DataContext : IdentityDbContext<IdentityUser>
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options)
+        {          
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set;}
+        public DbSet<Client> Clients {get; set;}
+        public DbSet<State> States {get; set;}
+        public DbSet<City> Cities {get; set;}
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryId);
+            
+            modelBuilder.Entity<Supplier>()
+                .HasOne(s => s.City)
+                .WithMany()
+                .HasForeignKey(s => s.CityId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Client>()
+                .HasOne(s => s.City)
+                .WithMany()
+                .HasForeignKey(s => s.CityId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<City>()
+                    .HasOne(c => c.State) // Cada cidade tem um estado
+                    .WithMany(s => s.Cities) // Um estado tem muitas cidades
+                    .HasForeignKey(c => c.StateId) // Chave estrangeira na tabela Cities
+                    .OnDelete(DeleteBehavior.Cascade); // Comportamento ao deletar (cascata)          
+        }
+
+
+
+       
+    }
+
+
+}
---
+++ SrbComercialInfrastructure/Data/DataContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SrbComercialInfrastructure.Data
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args) // Remova o valor padrão
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // Define o diretório atual
+                .AddJsonFile("appsettings.json") // Adiciona o arquivo de configurações
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder.UseMySql(configuration.GetConnectionString("DefaultConnection"),
+                new MySqlServerVersion(new Version(8, 0, 21))); // Altere a versão conforme necessário
+
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
